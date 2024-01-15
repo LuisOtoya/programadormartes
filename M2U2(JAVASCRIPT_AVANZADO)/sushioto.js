dba82b7590eebea1b0db94285b532658c18ad53d
Luis Otoya <luisotoya@gmail.com>
2024-01-15 02:00:50 -0300

    git push

--- M2U2(JAVASCRIPT_AVANZADO)/sushioto.js
+++ M2U2(JAVASCRIPT_AVANZADO)/sushioto.js
@@ -1,22 +1,27 @@
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', 
+        function() {
             const divElement = document.querySelector('div.horarios');
             divElement.addEventListener('click', function() {
                 divElement.style.color = '#0000FF';
             });
         })
-        document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', 
+        function() {
             const divElement = document.querySelector('div.titulo');
-            divElement.addEventListener('click', function() {
+            divElement.addEventListener('click', 
+        function() {
                 divElement.style.color = '#FF6347';
             });
         })
-        document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', 
+        function() {
             const divElement = document.querySelector('div.listas');
-            divElement.addEventListener('click', function() {
+            divElement.addEventListener('click', 
+        function() {
                 divElement.style.color = '#708090';
             });
         })
         function countCharacters(textarea) {
-        const charCount = document.getElementById("contador");
+            const charCount = document.getElementById("contador");
         charCount.textContent = textarea.value.length;
     }
