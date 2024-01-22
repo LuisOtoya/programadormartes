a171ba0a3da271142d6d64ebd1aeaea2ebd2bd3c
luisotoya1989@gmail.com <luisotoya1989@gmail.com>
2024-01-21 23:27:39 -0300

    update sushioto.js

--- M2U2(JAVASCRIPT_AVANZADO)/sushioto.js
+++ M2U2(JAVASCRIPT_AVANZADO)/sushioto.js
@@ -1,19 +1,28 @@
     document.addEventListener('DOMContentLoaded', function() {
             const divElement = document.querySelector('div.horarios');
             divElement.addEventListener('click', function() {
-                divElement.style.color = '#0000FF';
+                divElement.style.backgroundColor = '#0000FF';
+                setTimeout(function() {
+       divElement.style.backgroundColor = '';
+    }, 1000);
             });
         })
         document.addEventListener('DOMContentLoaded', function() {
             const divElement = document.querySelector('div.titulo');
             divElement.addEventListener('click', function() {
-                divElement.style.color = '#FF6347';
+                divElement.style.backgroundColor = '#FF6347';
+                setTimeout(function() {
+       divElement.style.backgroundColor = '';
+    }, 1000);
             });
         })
         document.addEventListener('DOMContentLoaded', function() {
             const divElement = document.querySelector('div.listas');
             divElement.addEventListener('click', function() {
-                divElement.style.color = '#708090';
+                divElement.style.backgroundColor = '#708090';
+                setTimeout(function() {
+       divElement.style.backgroundColor = '';
+    }, 1000);
             });
         })
         function countCharacters(textarea) {
