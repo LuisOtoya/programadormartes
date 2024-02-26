e5ccd86267402ad74a0467d47fce9fd8ada52258
Luis Otoya <luisotoya@gmail.com>
2024-02-26 01:27:51 -0300

    git commit m-

--- m3u3/app.js
+++ m3u3/app.js
@@ -3,15 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var exphbs = require('express-handlebars');
 
-var indexRouter = require('./routes/index');
+require('dotenv').config();
+
+var indexRouter = require('./routes/index'); // routes/index.js
 var usersRouter = require('./routes/users');
+var promosRouter = require('./routes/promos'); //routes/promos.js
+var cartasRouter = require('./routes/carta'); //routes/carta.js
+var nuestroespacioRouter = require('./routes/nuestroespacio'); //routes/nuestroespacio.js
+var direccionRouter = require('./routes/direccion'); //routes/direccion.js
+var redessocialesRouter = require('./routes/redessociales'); //routes/redessociales.js
+var deliveryRouter = require('./routes/delivery'); //routes/delivery.js
+var ingresarRouter = require('./routes/ingresar'); //routes/ingresar.js
+var contactoRouter = require('./routes/contacto'); //routes/contacto.js
 
 var app = express();
+var hbs = exphbs.create({
+  defaultLayout:'main'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs','exphbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,8 +35,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/promos', promosRouter);
+app.use('/carta', cartasRouter);
+app.use('/nuestroespacio', nuestroespacioRouter);
+app.use('/direccion', direccionRouter);
+app.use('/redessociales', redessocialesRouter);
+app.use('/delivery', deliveryRouter);
+app.use('/ingresar', ingresarRouter);
+app.use('/contacto', contactoRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
