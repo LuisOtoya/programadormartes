d710794b60626dcbf71c0c0982b5f26e9992552d
Luis Otoya <luisotoya@gmail.com>
2024-02-27 00:12:11 -0300

    git pull

--- m3u3/routes/nuestroespacio.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('esta es la pagina de nuestro espacio');
-});
-
-module.exports = router;
